f4f04ff5c290ab4f14434234a0f81d108f061cfd
MatteoL-petitordi <matteo.ftt@gmail.com>
2023-01-17 13:16:34 +0100

    🎨 Improving callbacks synthax

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <folder_watcher/folder_watcher.hpp>
+#include <iostream>
 
 auto main() -> int
 {
@@ -7,26 +7,19 @@ auto main() -> int
     auto watchedPath = std::filesystem::path(TEST_FOLDER);
 
     // Callbacks
-    folder_watcher::FolderWatcher_Callbacks callbacks;
-    callbacks.on_added_file = [](std::string_view path) {
-        std::cout << "file added " << path << std::endl;
-    };
-    callbacks.on_changed_file = [](std::string_view path) {
-        std::cout << "file changed " << path << std::endl;
-    };
-    callbacks.on_removed_file = [](std::string_view path) {
-        std::cout << "file removed " << path << std::endl;
-    };
-    callbacks.on_invalid_folder_path = [](std::string_view path) {
-        std::cout << "folder path invalid " << path << std::endl;
+    const folder_watcher::FolderWatcher_Callbacks callbacks{
+        .on_added_file          = [](std::string_view path) { std::cout << "file added " << path << std::endl; },
+        .on_removed_file        = [](std::string_view path) { std::cout << "file removed " << path << std::endl; },
+        .on_changed_file        = [](std::string_view path) { std::cout << "file changed " << path << std::endl; },
+        .on_invalid_folder_path = [](std::string_view path) { std::cout << "folder path invalid " << path << std::endl; },
     };
 
     // Create the folder path
     folder_watcher::FolderWatcher folder_watcher{watchedPath, callbacks};
 
-    while (true) {
+    while (true)
+    {
         // Loop on update
         folder_watcher.update();
     };
 }
-
