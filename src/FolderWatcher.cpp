7927d2e661aac69863124113c41de35c8aa2fb34
MatteoL-petitordi <matteo.ftt@gmail.com>
2023-01-18 11:04:55 +0100

    🧼 Cleaning Folder Watcher and update() method

--- src/FolderWatcher.cpp
+++ src/FolderWatcher.cpp
@@ -2,163 +2,130 @@
 #include <algorithm>
 #include <filesystem>
 #include <iostream>
-#include "./utils.hpp"
 
 namespace folder_watcher {
 
-using RecursiveDirectoryIterator = fs::recursive_directory_iterator;
-using DirectoryIterator          = fs::directory_iterator;
-using Clock                      = std::chrono::steady_clock;
-
 FolderWatcher::FolderWatcher(fs::path folder_path, Config config)
-    : _path(std::move(folder_path)), _folder_last_change(time_of_last_change(_path)), _config(config)
+    : _path{std::move(folder_path)}
+    , _config{config}
 {
-    update_path_validity();
+    check_and_store_path_existence();
 };
 
 void FolderWatcher::update(Callbacks const& callbacks) const
 {
-    if (has_check_too_recently())
+    if (has_checked_too_recently() || has_invalid_folder_path(callbacks))
         return;
 
-    bool const was_valid = _path_validity;
-    update_path_validity();
+    watch_for_edit_and_remove(callbacks);
 
-    if (!_path_validity)
+    watch_for_new_paths(callbacks);
+
+    _folder_last_change = get_time_of_last_change(_path);
+}
+
+void FolderWatcher::set_folder_path(Callbacks const& callbacks, fs::path const& path)
+{
+    _path = path;
+    check_and_store_path_existence();
+    remove_files(callbacks, _files); // Clear all files because the path changed and we might not need to watch them anymore.
+};
+
+auto FolderWatcher::has_invalid_folder_path(Callbacks const& callbacks) const -> bool
+{
+    bool const was_valid = _path_exists;
+    check_and_store_path_existence();
+
+    if (!_path_exists && was_valid)
     {
-        if (was_valid)
-            on_folder_path_invalid(callbacks);
-        return;
+        callbacks.on_invalid_folder_path(_path);
+        remove_files(callbacks, _files);
+        return true;
     }
 
-    /*if (!was_valid && !_path_validity)
-        return;
+    if (!_path_exists)
+        return true;
 
-    if (was_valid && !_path_validity) {
-        on_folder_path_invalid(callbacks);
-        return;
-    }*/
+    return false;
+}
 
-    std::vector<File> will_be_removed{};
+void FolderWatcher::check_and_store_path_existence() const
+{
+    _path_exists = fs::exists(_path);
+}
+
+void FolderWatcher::watch_for_edit_and_remove(Callbacks const& callbacks) const
+{
+    auto to_remove = std::vector<File>{};
     for (File& file : _files)
     {
-        // Files deleted
-        if (!fs::exists(file.path))
+        // File deleted
+        if (!fs::exists(file.path()))
         {
-            will_be_removed.push_back(file);
+            to_remove.push_back(file);
             continue;
         }
 
         // File changed
-        const auto last_change = time_of_last_change(file.path);
-        if (last_change != file.time_of_last_change)
+        auto const last_change = get_time_of_last_change(file.path());
+        if (last_change != file.time_of_last_change())
         {
-            on_changed_file(callbacks, file);
-            continue;
+            file.time_of_last_change(last_change);
+            callbacks.on_file_changed(file.path());
         }
     }
-
-    remove_files(callbacks, will_be_removed);
-    check_for_new_paths(callbacks);
-    _folder_last_change = time_of_last_change(_path);
+    remove_files(callbacks, to_remove);
 }
 
-[[maybe_unused]] void FolderWatcher::set_folder_path(Callbacks const& callbacks, fs::path path)
-{
-    _path = std::move(path);
-    update_path_validity();
-    remove_files(callbacks, _files);
-};
-
-void FolderWatcher::update_path_validity() const
-{
-    _path_validity = fs::exists(_path);
-}
-
-void FolderWatcher::check_for_new_paths(Callbacks const& callbacks) const
+void FolderWatcher::watch_for_new_paths(Callbacks const& callbacks) const
 {
     if (_config.watch_all_subfolders_recursively)
     {
-        for (const auto& entry : RecursiveDirectoryIterator(_path))
-        {
-            add_to_files(callbacks, entry);
-        }
+        for (auto const& entry : fs::recursive_directory_iterator(_path))
+            add_to_files_if_necessary(callbacks, entry);
     }
-
     else
     {
-        for (const auto& entry : DirectoryIterator(_path))
-        {
-            add_to_files(callbacks, entry);
-        }
+        for (auto const& entry : fs::directory_iterator(_path))
+            add_to_files_if_necessary(callbacks, entry);
     }
 }
 
-void FolderWatcher::remove_files(Callbacks const& callbacks, std::vector<File>& will_be_removed) const
+void FolderWatcher::add_to_files_if_necessary(Callbacks const& callbacks, fs::directory_entry const& entry) const
 {
-    if (will_be_removed.empty())
-        return;
-
-    _files.erase(
-        std::remove_if(_files.begin(), _files.end(), [&will_be_removed](const File& f) {
-            return std::find(will_be_removed.begin(), will_be_removed.end(), f) != will_be_removed.end();
-        }),
-        _files.end()
-    );
-
-    for (File const& file : will_be_removed)
-        on_removed_file(callbacks, file);
-}
-
-void FolderWatcher::add_to_files(Callbacks const& callbacks, const fs::directory_entry& entry) const
-{
-    if (entry.is_directory())
+    if (!entry.is_regular_file() && !entry.is_symlink())
         return;
 
     // If the file is not found in _files, we add it
-    auto const file_iterator = std::find_if(_files.begin(), _files.end(), [entry](File const& file) { return file.path == entry; });
-    if (file_iterator == _files.end())
-        on_added_file(callbacks, entry.path());
+    auto const file_iterator = std::find_if(_files.begin(), _files.end(), [&entry](File const& file) { return file.path() == entry; });
+    if (file_iterator != _files.end())
+        return;
+
+    _files.emplace_back(entry);
+    callbacks.on_file_added(entry);
 }
 
-auto FolderWatcher::has_check_too_recently() const -> bool
+void FolderWatcher::remove_files(Callbacks const& callbacks, std::vector<File>& to_remove) const
 {
-    static auto last_check   = Clock::now();
-    const auto  now          = Clock::now();
-    const auto  elapsed_time = std::chrono::duration<float>{now - last_check};
+    for (File const& file : to_remove)
+        callbacks.on_file_removed(file.path());
+
+    // Erase all common files in to_remove and in _files
+    std::erase_if(_files, [&to_remove](File const& f) {
+        return std::find(to_remove.begin(), to_remove.end(), f) != to_remove.end();
+    });
+}
+
+auto FolderWatcher::has_checked_too_recently() const -> bool
+{
+    const auto now          = Clock::now();
+    const auto elapsed_time = std::chrono::duration<float>{now - _folder_last_check};
 
     if (elapsed_time.count() < _config.seconds_between_checks)
         return true;
 
-    last_check = now;
+    _folder_last_check = now;
     return false;
-}
-
-void FolderWatcher::on_added_file(Callbacks const& callbacks, const fs::path& path) const
-{
-    if (fs::is_directory(path))
-        return;
-
-    _files.push_back({.path = path, .time_of_last_change = time_of_last_change(path)});
-    callbacks.on_file_added(_files.back().path.string());
-}
-
-void FolderWatcher::on_removed_file(Callbacks const& callbacks, File const& file) const
-{
-    callbacks.on_file_removed(file.path.string());
-    _files.erase(std::remove_if(_files.begin(), _files.end(), [file](const File& cur_file) { return file == cur_file; }), _files.end());
-}
-
-void FolderWatcher::on_changed_file(Callbacks const& callbacks, File& file) const
-{
-    file.time_of_last_change = time_of_last_change(file.path);
-    callbacks.on_file_changed(file.path.string());
-}
-
-void FolderWatcher::on_folder_path_invalid(Callbacks const& callbacks) const
-{
-    callbacks.on_invalid_folder_path(_path.string());
-    remove_files(callbacks, _files);
 }
 
 } // namespace folder_watcher
--- src/FolderWatcher.hpp
+++ src/FolderWatcher.hpp
@@ -5,8 +5,12 @@
 #include <utility>
 #include <variant>
 #include <vector>
+#include "./File.hpp"
+#include "./utils.hpp"
 
 namespace folder_watcher {
+
+using Clock = std::chrono::steady_clock;
 
 struct Config {
     /// If false, only watches the files that are direct childs of the watched path.
@@ -19,10 +23,10 @@ struct Config {
 };
 
 struct Callbacks {
-    std::function<void(std::string_view)> on_file_added;
-    std::function<void(std::string_view)> on_file_removed;
-    std::function<void(std::string_view)> on_file_changed;
-    std::function<void(std::string_view)> on_invalid_folder_path;
+    std::function<void(std::filesystem::path)> on_file_added;
+    std::function<void(std::filesystem::path)> on_file_removed;
+    std::function<void(std::filesystem::path)> on_file_changed;
+    std::function<void(std::filesystem::path)> on_invalid_folder_path;
 };
 
 namespace fs = std::filesystem;
@@ -35,46 +39,35 @@ public:
     /// It will call the callbacks whenever an event occurs.
     void update(Callbacks const& = {}) const;
 
-    // [[nodiscard]] constexpr auto        is_folder_path_invalid() const -> bool { return !_path_validity; };
-    [[nodiscard]] constexpr auto        is_folder_path_valid() const -> bool { return _path_validity; };
-    [[maybe_unused]] [[nodiscard]] auto get_folder_path() const -> fs::path { return _path; }
-    [[maybe_unused]] void               set_folder_path(Callbacks const&, fs::path);
+    [[nodiscard]] auto is_folder_path_valid() const -> bool { return _path_exists; };
+    [[nodiscard]] auto get_folder_path() const -> fs::path const& { return _path; }
+    void               set_folder_path(Callbacks const&, fs::path const&);
 
 private:
-    struct File {
-        fs::path           path;
-        fs::file_time_type time_of_last_change;
-        friend auto        operator==(File const& a, File const& b) -> bool { return a.path == b.path; }
-    };
+    auto has_invalid_folder_path(Callbacks const&) const -> bool;
 
-private:
-    /// Check if the folder path exists and set it into _path_validity
-    void update_path_validity() const;
+    void check_and_store_path_existence() const;
+
+    void watch_for_edit_and_remove(Callbacks const&) const;
 
     /// This method compares the content of the folder and the content of the _files vector.
-    /// If a file is not listed in _files, we add it by calling `add_to_files`
-    void check_for_new_paths(Callbacks const&) const;
-
-    /// Removes a vector of <File> from the _files attribute.
-    void remove_files(Callbacks const&, std::vector<File>& will_be_removed) const;
+    void watch_for_new_paths(Callbacks const&) const;
 
     /// Add a path to the _files attribute by calling `on_added_file` method.
-    void add_to_files(Callbacks const&, const fs::directory_entry&) const;
+    void add_to_files_if_necessary(Callbacks const&, const fs::directory_entry&) const;
+
+    /// Removes a vector of `File`s from _files.
+    void remove_files(Callbacks const&, std::vector<File>& to_remove) const;
 
     /// Verifies is the update method should be executed or not.
-    /// It uses the FolderWatcher_Config::delay_between_checks
-    [[nodiscard]] auto has_check_too_recently() const -> bool;
-
-    /// These methods call the corresponding callback.
-    void on_added_file(Callbacks const&, fs::path const&) const;
-    void on_removed_file(Callbacks const&, File const&) const;
-    void on_changed_file(Callbacks const&, File&) const;
-    void on_folder_path_invalid(Callbacks const&) const;
+    /// It uses the Config::delay_between_checks
+    [[nodiscard]] auto has_checked_too_recently() const -> bool;
 
 private:
     fs::path                   _path{};
     mutable fs::file_time_type _folder_last_change{};
-    mutable bool               _path_validity{};
+    mutable Clock::time_point  _folder_last_check{};
+    mutable bool               _path_exists{};
     Config                     _config{};
     mutable std::vector<File>  _files{};
 };
