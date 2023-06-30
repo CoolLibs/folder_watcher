03ff003c056164820b835745b33c53c0cff398a0
Jules Fouchy <jules.fouchy@ntymail.com>
2023-06-30 19:33:37 +0200

    🐛 [tests] Fix cout not refreshing sometimes

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -7,10 +7,10 @@ auto main() -> int
     while (true)
     {
         folder_watcher.update({
-            .on_file_added          = [](std::filesystem::path const& path) { std::cout << "file added " << path.string() << '\n'; },
-            .on_file_removed        = [](std::filesystem::path const& path) { std::cout << "file removed " << path.string() << '\n'; },
-            .on_file_changed        = [](std::filesystem::path const& path) { std::cout << "file changed " << path.string() << '\n'; },
-            .on_invalid_folder_path = [](std::filesystem::path const& path) { std::cout << "folder path invalid " << path.string() << '\n'; },
+            .on_file_added          = [](std::filesystem::path const& path) { std::cout << "file added " << path.string() << std::endl; },
+            .on_file_removed        = [](std::filesystem::path const& path) { std::cout << "file removed " << path.string() << std::endl; },
+            .on_file_changed        = [](std::filesystem::path const& path) { std::cout << "file changed " << path.string() << std::endl; },
+            .on_invalid_folder_path = [](std::filesystem::path const& path) { std::cout << "folder path invalid " << path.string() << std::endl; },
         });
     }
 }
